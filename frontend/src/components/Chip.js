0bd06c9ad0344687fb98186c6d1c4c1dd3cf8ee6
Jooeun Park <civbf1@gmail.com>
2024-04-26 14:25:43 -0700

    Finalize detail

--- frontend/src/components/Chip.js
+++ frontend/src/components/Chip.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import './chip.css';
 
-const Chip = ({ content, width, onClick, clicked }) => {
+const Chip = ({ content, width, onClick, clicked, filled }) => {
     return (
         <div
-            className={`chip ${clicked ? 'clicked' : ''}`}
+            className={`chip ${clicked ? 'clicked' : ''} ${clicked&&filled? 'filled':''}`}
             style={{width: width}}
             onClick={onClick} 
         >
