bca52a381e7e8ab77d29c14f227e1d74eafa4f74
Jooeun Park <civbf1@gmail.com>
2024-04-27 22:53:15 -0700

    Added shadowing

--- frontend/src/screen/VideoDetail.js
+++ frontend/src/screen/VideoDetail.js
@@ -5,6 +5,14 @@ const VideoDetail = ({ scripts, url, step, isModalOpen }) => {
   const videoRef = useRef(null);
   const [player, setPlayer] = useState(null);
   const [displayedScripts, setDisplayedScripts] = useState([]);
+  const [endScriptTime, setEndScriptTime] = useState(null);
+  const stepRef = useRef(step);
+  const endScriptTimeRef = useRef(endScriptTime);
+  const repeatCountRef = useRef(0); 
+  const durRef = useRef(0);
+  
+
+  
 
   const stages = [
     { title: '자막없이 풀기', type: "video" },
@@ -14,11 +22,18 @@ const VideoDetail = ({ scripts, url, step, isModalOpen }) => {
   ]
 
   useEffect(() => {
+    stepRef.current = step;
+    endScriptTimeRef.current = endScriptTime;
+  }, [step, endScriptTime]);
+  
+  useEffect(() => {
     const initializePlayer = () => {
       
       const onPlayerReady = (event) => {
         setPlayer(event.target);
+        event.target.addEventListener('onStateChange', function(){});
       };
+      
       new window.YT.Player(videoRef.current, {
         width: '370',
         height: '219',
@@ -26,7 +41,6 @@ const VideoDetail = ({ scripts, url, step, isModalOpen }) => {
         events: {
           'onReady': onPlayerReady,
           'onStateChange': onPlayerStateChange,
-          'controls': 0, 
         }
       });
     };
@@ -56,6 +70,7 @@ const VideoDetail = ({ scripts, url, step, isModalOpen }) => {
     let intervalId;
 
     if (player && step === 1) {
+
       intervalId = setInterval(() => {
         const currentTime = player.getCurrentTime();
         const newScripts = scripts.filter(script => {
@@ -72,14 +87,70 @@ const VideoDetail = ({ scripts, url, step, isModalOpen }) => {
     return () => clearInterval(intervalId);
   }, [player, scripts, displayedScripts, step]);
 
-  const onPlayerStateChange = (event) => {
-    if (step === 0 || step === 3) {
-      if (event.data === window.YT.PlayerState.ENDED) {
-        isModalOpen(true);
-      }
-    }
-  };
 
+  useEffect(() => {
+    if (step === 1) {
+      alert("자막을 클릭해서 해당 구간을 반복해서 볼 수 있습니다. 반복 이후에는 쉐도잉 시간 5초가 주어집니다. 1회 반복합니다.");
+    }
+  }, [step]);
+  
+const onPlayerStateChange = (event) => {
+  if (stepRef.current === 0 || stepRef.current === 3) {
+    if (event.data === window.YT.PlayerState.ENDED) {
+      isModalOpen(true);
+    }
+  }
+
+
+
+
+  if (event.data === window.YT.PlayerState.PLAYING) {
+    if(endScriptTimeRef.current && repeatCountRef.current > 0) {
+      const checkEndTimeAndPause = () => {
+        const currentTime = event.target.getCurrentTime();
+        if (currentTime >= endScriptTimeRef.current-0.6) {
+          event.target.pauseVideo();
+          
+          setTimeout(() => {
+            if (event.target) {
+              if (repeatCountRef.current > 0) {
+                
+              if (repeatCountRef.current > 1) {
+                event.target.seekTo(endScriptTimeRef.current - parseFloat(endScriptTimeRef.current) - parseFloat(durRef), true);
+              }
+                event.target.playVideo();
+                repeatCountRef.current -= 1; 
+                if (repeatCountRef.current <= 0) {
+                  
+                  setEndScriptTime(null); 
+                }
+              } else {
+                event.target.playVideo();
+              }
+            }
+          }, 4500);
+          clearInterval(intervalId);
+        }
+      };
+
+      const intervalId = setInterval(checkEndTimeAndPause, 1000);
+    }
+  }
+};
+
+const rewindVideoToScriptSegment = (start, dur) => {
+  if (player) {
+    player.playVideo();
+    const startTimeSeconds = parseFloat(start);
+    const durationSeconds = parseFloat(dur);
+    durRef.current=dur;
+    const endTimeSeconds = startTimeSeconds + durationSeconds;
+    player.seekTo(startTimeSeconds, true);
+    setEndScriptTime(endTimeSeconds);
+    repeatCountRef.current = 2; 
+  }
+}
+  
   return (
     <div className='video-detail'>
       <h3>STEP {step + 1}: {stages[step].title}</h3>
@@ -89,7 +160,11 @@ const VideoDetail = ({ scripts, url, step, isModalOpen }) => {
       {step === 1 ? <div className='scripts-wrapper'>
         {
           displayedScripts.map((item, key) => (
-            <div className='script' key={key}>
+            <div className='script' key={key}
+            onClick={() => rewindVideoToScriptSegment(item.start, item.dur)}
+            style={{cursor: "pointer"}}
+
+            >
               <div className='script-num'>{key + 1}/{scripts.length}</div>
               <div className='script-content'>{item.text}</div>
             </div>
